d797b5b1659ce28f7333251c0240c5c42b993b48
toan215 <toantruongcong2005bee@gmail.com>
2024-06-14 14:46:01 +0700

    .

--- script.js
+++ script.js
@@ -90,3 +90,13 @@ checkOrientation();
 
 // Add event listener for orientation change
 window.addEventListener('resize', checkOrientation);
+
+function redirectOnScrollOrOrientationChange() {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    window.location.href = "plane.html";
+  }
+}
+
+window.addEventListener("scroll", redirectOnScrollOrOrientationChange);
+window.addEventListener("resize", redirectOnScrollOrOrientationChange); 
+window.addEventListener("orientationchange", redirectOnScrollOrOrientationChange);
