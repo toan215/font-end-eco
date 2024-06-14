32a1b49665e94abeb8bf2e203c70394decae3fb8
toan215 <toantruongcong2005bee@gmail.com>
2024-06-14 14:22:07 +0700

    responsive fix

--- script.js
+++ script.js
@@ -73,3 +73,20 @@ document.addEventListener("scroll", function() {
       window.location.href = "plane.html";
   }
 });
+function checkOrientation() {
+  if (window.innerHeight > window.innerWidth) {
+      // Portrait mode
+      document.getElementById('rotate-message').style.display = 'flex';
+      document.getElementById('contents').style.display = 'none';
+  } else {
+      // Landscape mode
+      document.getElementById('rotate-message').style.display = 'none';
+      document.getElementById('contents').style.display = 'block';
+  }
+}
+
+// Initial check
+checkOrientation();
+
+// Add event listener for orientation change
+window.addEventListener('resize', checkOrientation);
