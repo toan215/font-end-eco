2c6c7f3ce0d9837a4c36a817434392ba0b81fd77
toan215 <toantruongcong2005bee@gmail.com>
2024-07-11 22:05:49 +0700

    1.3

--- js/login.js
+++ js/login.js
@@ -21,19 +21,3 @@ const bcrypt = require('bcrypt');
 
 const app = express();
 
-//use ejs as view engine
-app.set('view.egine', 'ejs');
-
-app.get("/", (req, res) => {
-	res.render("login");
-});
-
-app.get("/signup", (req, res) => {
-	res.render("signup");
-});
-
-
-const port = 5000;
-app.listen(port,() => {
-    console.log('Server running on Port : ${port}');
-})
